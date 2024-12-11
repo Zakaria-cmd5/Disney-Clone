bda64f0dc36ee0b8422a57fb4bcc0ce3bd30eb3b
Zakaria Asad <zasad2587@gmail.com>
2024-12-11 21:27:43 +0200

    Build the movies card and fetched it in the home page from the backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button";
+import MoviesCarousel from "@/components/homePage/MoviesCarousel";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 
-export default function Home() {
+export default async function Home() {
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
+
   return (
     <div>
-      <Button>Click Me</Button>
+      <div className="flex flex-col space-y-2 xl:my-48">
+        <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
+        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousel movies={popularMovies} title="Popular" />
+      </div>
     </div>
   );
 }
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,27 @@ export type Genre = {
 export type Genres = {
   genres: Genre[];
 };
+
+export type Movie = {
+  id: number;
+  title: string;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  release_date:string
+};
+
+export type SearchResults = {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
